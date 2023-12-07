a78413d2681fee8a87aeda83406a5fb1e1e13db3
IanLalonde <lali3401@usherbrooke.ca>
2023-12-07 16:10:53 -0500

    basic jackal ros node with launch file

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# jackal
-custom ros program for the Clearpath Jackal
+# Jackal ROS Control Script
+
+## Overview
+This Python script provides a basic obstacle avoidance and safety control mechanism for a Jackal robot in a ROS (Robot Operating System) environment. The script subscribes to laser scan data, monitors the surroundings, and adjusts the robot's velocity commands to avoid collisions.
+
+## Requirements
+- ROS (Robot Operating System)
+- Python 2.7 or later
+- NumPy
+- Jackal simulation environment
+
+## Installation
+1. Ensure that ROS is installed on your system.
+2. Clone this repository into your ROS workspace.
+   ~~~bash
+   git clone https://github.com/your-username/your-repository.git
+   ~~~
+3. Build your ROS workspace.
+   ~~~bash
+   cd your-ros-workspace
+   catkin_make
+   ~~~
+
+## Usage
+1. Run the control script:
+   ~~~bash
+   roslaunch basic_pkg start_basic.launch 
+   ~~~
+2. Launch the Gazebo simulator for Jackal with the following command:
+   ~~~bash
+   roslaunch jackal_gazebo jackal_world.launch config:=front_laser
+   ~~~
+
+
+
+## Node Details
+- **Node Name:** control_test
+- **Subscribed Topics:**
+  - `/scan`: Laser scan data from the robot's sensors.
+  - `/front/scan`: Laser scan data specifically for the front portion of the robot.
+  - `/twist_marker_server/cmd_vel`: Velocity commands from the twist marker server.
+  - `/bluetooth_teleop/cmd_vel`: Velocity commands from Bluetooth teleoperation.
+  - `/joy_teleop/cmd_vel`: Velocity commands from joystick teleoperation.
+  - `/jackal_velocity_controller/odom`: Odometry data for monitoring the Jackal's speed.
+
+## Parameters
+- `front_scan_point`: Number of points used for the front laser scanner.
+
+## Behavior
+The script continuously monitors the laser scan data to detect obstacles in front of the Jackal. If an obstacle is too close (`safe_distance`), it triggers a safety mode, reducing or stopping the robot's linear velocity. The control script can be adjusted for specific applications by modifying the parameters and tuning the reaction time, decceleration, and other control parameters.
+
+Feel free to customize and extend this script based on your specific requirements.
--- basic_pkg/src/exemple.py
+++ basic_pkg/src/exemple.py
@@ -4,18 +4,6 @@
 Creator: Ian Lalonde, Charles-Alexis Lavoie
 Date:    24-11-2023
 
-needs to replace husky_control/config/twist_mux.yaml ->
-
-topics:
-- name    : con_vel
-  topic   : con_vel
-  timeout : 0.5
-  priority: 10
-locks:
-- name    : e_stop
-  topic   : e_stop
-  timeout : 0.0
-  priority: 255
 
 launch gazebo for jackal with this command: roslaunch jackal_gazebo jackal_world.launch config:=front_laser
 
@@ -29,7 +17,7 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
 class ControlTest:
-	def __init__(self, front_scan_point,rate):
+	def __init__(self, front_scan_point):
 		self.front_scan_point      	= front_scan_point
 		self.front_scan            	= np.zeros(front_scan_point)
 		self.safe                  	= True
@@ -50,21 +38,21 @@ class ControlTest:
 		self.saturation_distance   	= 2. * self.reaction_time * self.max_vehicle_speed
 
 		#publishing
-		self.pub                   	= rospy.Publisher('con_vel', Twist, queue_size=10)
+		self.pub                   	= rospy.Publisher('con_vel', Twist, queue_size=10) #con_vel, change priority in twist_mux.yaml to overwrite other com_vels
 		self.pub_msg               	= Twist()
 		
-		self.subScan			   	= rospy.Subscriber("scan", LaserScan, self.callback_scan)
-		self.subFrontScan		   	= rospy.Subscriber("front/scan", LaserScan, self.callback_scan)
-		self.subCmd_vel			   	= rospy.Subscriber("cmd_vel", Twist, self.callback_cmd)
-		self.subTwistCmd_vel	   	= rospy.Subscriber("twist_marker_server/cmd_vel", Twist, self.callback_cmd)
-		self.subBtCmd_vel		   	= rospy.Subscriber("bluetooth_teleop/cmd_vel", Twist, self.callback_cmd)
-		self.subJoyCmd_vel		   	= rospy.Subscriber("joy_teleop/cmd_vel", Twist, self.callback_cmd)
-		self.subOdom 			   	= rospy.Subscriber("jackal_velocity_controller/odom", Odometry, self.callback_odom)
+		self.subScan			= rospy.Subscriber("scan", LaserScan, self.callback_scan)
+		self.subFrontScan		= rospy.Subscriber("front/scan", LaserScan, self.callback_scan)
+		#self.subCmd_vel		= rospy.Subscriber("cmd_vel", Twist, self.callback_cmd)
+		self.subTwistCmd_vel	= rospy.Subscriber("twist_marker_server/cmd_vel", Twist, self.callback_cmd)
+		self.subBtCmd_vel		= rospy.Subscriber("bluetooth_teleop/cmd_vel", Twist, self.callback_cmd)
+		self.subJoyCmd_vel		= rospy.Subscriber("joy_teleop/cmd_vel", Twist, self.callback_cmd)
+		self.subOdom 			= rospy.Subscriber("jackal_velocity_controller/odom", Odometry, self.callback_odom)
 		
-        self.dt						= 1/rate
-        self.timer					= rospy.Timer( rospy.Duration( self.dt ), self.publish_loop )
+		self.dt				= 0.01
+		self.timer			= rospy.Timer( rospy.Duration( self.dt ), self.publish_loop )
 	
-		self.lock_input				= False
+		self.lock_input			= False
 
 	
 	#check front scanner
@@ -79,72 +67,46 @@ class ControlTest:
 			self.last_linear_cmd = data.linear.x
 		self.last_angular_cmd = data.angular.z
 
-	#constantly check husky speed
+	#constantly check jackal speed
 	def callback_odom(self, data):
 		self.vehicle_speed = data.twist.twist.linear.x
-		if self.vehicle_speed > self.max_vehicle_speed:
-			self.max_vehicle_speed = self.vehicle_speed		
-		self.saturation_distance = 2 * self.reaction_time * self.vehicle_speed
-		if self.saturation_distance < self.safe_distance:
-			self.saturation_distance = self.safe_distance
+		
 
 	#Stop the robot if he is too close to something
-	def publish_loop(self):
+	def publish_loop(self, timer):
 		while not rospy.is_shutdown():
 			self.is_safe()
-			self.is_safe_rotate()
-			if  not self.safe and not self.saturation and  not self.safe_rotate:
-				print('red')
-				self.linear_cmd = 0
-				self.pub_msg.linear.x = self.linear_cmd 
-				self.angular_cmd = self.last_angular_cmd
-			elif self.saturation and self.safe:
-				print('yellow')
-				dec_fac1 = (1.0/ (   (2.0*self.reaction_time*self.vehicle_speed)-self.safe_distance)) * self.min_distance
-				dec_fac2 = self.safe_distance/((2.*self.reaction_time*self.vehicle_speed) -self.safe_distance)   
-				self.linear_cmd = self.last_linear_cmd * (dec_fac1-dec_fac2)
-				self.last_linear_cmd = self.linear_cmd
-				self.angular_cmd = self.last_angular_cmd 
-			else:
-				print('green')
+			if self.safe:
+				#print('green')
 				self.linear_cmd = self.last_linear_cmd
+				self.angular_cmd = self.last_angular_cmd
+
+			else :
+				#print('red')
+				self.linear_cmd = min(0, self.last_linear_cmd)
+				self.pub_msg.linear.x = self.linear_cmd 
 				self.angular_cmd = self.last_angular_cmd
 
 			self.pub_msg.linear.x = self.linear_cmd 
 			self.pub_msg.angular.z = self.angular_cmd 
-			self.pub.publish(self.pub_msg)				
-			self.rate.sleep()
+			self.pub.publish(self.pub_msg)			
 
 	#check if something is in front of the robot
 	def is_safe(self):
-		#zone rouge stop
+		#danger zone
 		if self.min_distance < self.safe_distance:
-			self.lock_input = True
 			self.safe = False
-			self.safe_rotate = False
-			self.saturation = False 
-		#zone jaune decceleration
-		elif self.min_distance > self.safe_distance and self.min_distance < self.saturation_distance:	
-			self.lock_input = True 
-			self.safe = True
-			self.saturation = True 	
-		#zone verte libre
+		#safe zone
 		else:	
-			self.lock_input = False
 			self.safe = True
-			self.saturation = False		
 
-	#check if the user is trying to rotate and can rotate
-	def is_safe_rotate(self):
-		if not self.safe and not self.safe_rotate and (self.last_angular_cmd is not 0. or self.last_linear_cmd < 0.) and self.last_linear_cmd < 0.20:
-			self.safe_rotate = True
 	
 			
 #rate              = publishing rate of the new cmd
 #front_scan_point  = Number of point for the front scanner
 if __name__ == '__main__':           
 	rospy.init_node('control_test')                 
-	node = ControlTest(front_scan_point=30, rate=100)
+	node = ControlTest(front_scan_point=30)
 	rospy.spin()
 	
 
